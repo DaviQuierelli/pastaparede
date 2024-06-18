9a7dc8005770bb05d2b8f97e640451e4208795a2
DaviQuierelli <davi.quierelli@gmail.com>
2024-06-18 19:37:37 -0300

    parede4

--- projparede/wsgi.py
+++ projparede/wsgi.py
@@ -14,4 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projparede.settings')
 
 application = get_wsgi_application()
-app = get_wsgi_application()
+projparede = get_wsgi_application()
